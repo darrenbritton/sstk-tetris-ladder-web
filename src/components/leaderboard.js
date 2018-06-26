b3b1c7e311355fc939ccc158ca066405d7635596
Darren Britton <darrenbritton@hotmail.com>
2018-06-27 00:26:19 +0100

    fix: stop sort order from changing on equal sort elements

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -41,8 +41,8 @@ const columnData = [
 
 function getSorting(order, orderBy) {
   return order === 'desc'
-    ? (a, b) => (b[orderBy] < a[orderBy] ? -1 : 1)
-    : (a, b) => (a[orderBy] < b[orderBy] ? -1 : 1);
+    ? (a, b) => (b[orderBy] < a[orderBy] ? -1 : b[orderBy] === a[orderBy] ? 0 : 1)
+    : (a, b) => (a[orderBy] < b[orderBy] ? -1 : a[orderBy] === b[orderBy] ? 0 : 1);
 }
 
 class Leaderboard extends Component {
